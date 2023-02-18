856ad732759198a3b03d2db3266a96e205744e47
Sandro Park <ticoba@naver.com>
2023-02-18 17:26:24 +0900

    Prime factors : 3은 3을 반환한다.

--- subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
@@ -11,20 +11,25 @@ class PrimeFactorsTest {
 
     @Test
     void canFactorIntoPrimes() throws Exception {
-        assertThat(of(1)).isEqualTo(list());
-        assertThat(of(2)).isEqualTo(list(2));
-    }
-
-    private List<Integer> list(Integer... ints) {
-        return List.of(ints);
+        assertPrimeFactors(1, list());
+        assertPrimeFactors(2, list(2));
+        assertPrimeFactors(3, list(3));
     }
 
     private List<Integer> of(int n) {
         List<Integer> factors = new ArrayList<>();
         if (n > 1) {
-            factors.add(2);
+            factors.add(n);
         }
         return factors;
+    }
+
+    private void assertPrimeFactors(int n, List<Integer> list) {
+        assertThat(of(n)).isEqualTo(list);
+    }
+
+    private List<Integer> list(Integer... ints) {
+        return List.of(ints);
     }
 
 }
