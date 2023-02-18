6f085f390554acd209e244cc10df49e49626cd6e
Sandro Park <ticoba@naver.com>
2023-02-19 07:56:40 +0900

    TDD로 xUnit 만들기 : SetUp
    - 테스트 메서드가 실행되기 전 먼저 실행되는 메서드인 SetUp을 생성했다.

--- subject/xUnit-TDD/src/main/java/com/sandro/xunit/WasRun.java
+++ subject/xUnit-TDD/src/main/java/com/sandro/xunit/WasRun.java
@@ -2,9 +2,15 @@ package com.sandro.xunit;
 
 public class WasRun extends TestCase {
     public boolean wasRun;
+    public boolean wasSetUp;
 
     public WasRun(String name) {
         super(name);
+    }
+
+    @Override
+    protected void setUp() {
+        wasSetUp = true;
     }
 
     public void testMethod() {
--- subject/xUnit-TDD/src/main/java/com/sandro/xunit/XUnitTest.java
+++ subject/xUnit-TDD/src/main/java/com/sandro/xunit/XUnitTest.java
@@ -3,5 +3,6 @@ package com.sandro.xunit;
 public class XUnitTest {
     public static void main(String[] args) {
         new TestCaseTest("testRunning").run();
+        new TestCaseTest("testSetUp").run();
     }
 }
