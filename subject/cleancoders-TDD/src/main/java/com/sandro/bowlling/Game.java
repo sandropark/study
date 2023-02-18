6d495368d845cd220d1cd715c45fd581f0f40f32
Sandro Park <ticoba@naver.com>
2023-02-18 11:49:30 +0900

    볼링 게임 : 공 굴리기와 거터게임

---
+++ subject/cleancoders-TDD/src/main/java/com/sandro/bowlling/Game.java
@@ -0,0 +1,15 @@
+package com.sandro.bowlling;
+
+public class Game {
+
+    /**
+     * 공 굴리기
+     * @param pins 쓰러트린 핀 수
+     */
+    public void roll(int pins) {
+    }
+
+    public int getScore() {
+        return 0;
+    }
+}
--- subject/cleancoders-TDD/src/test/java/com/sandro/bowlling/GameTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/bowlling/GameTest.java
@@ -1,4 +1,34 @@
 package com.sandro.bowlling;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+
+    @DisplayName("공을 굴린다.")
+    @Test
+    void canRoll() throws Exception {
+        game.roll(0);
+    }
+
+    @DisplayName("한 번도 핀을 쓰러트리지 못하는 경우 0점을 반환한다.")
+    @Test
+    void gutterGame() throws Exception {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertThat(game.getScore()).isEqualTo(0);
+    }
+
 }
