1e8099198a914e71a0fd94b44765cca7fbe750ce
Sandro Park <ticoba@naver.com>
2023-02-18 18:04:26 +0900

    WordWrapper : 새로운 테스트 케이스 작성

--- subject/cleancoders-TDD/src/test/java/com/sandro/wordwrapper/WrapperTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/wordwrapper/WrapperTest.java
@@ -10,6 +10,7 @@ class WrapperTest {
     void test() throws Exception {
         assertThat(wrap("word word", 4)).isEqualTo("word\nword");
         assertThat(wrap("a dog", 5)).isEqualTo("a dog");
+        assertThat(wrap("a dog with a bone", 6)).isEqualTo("a dog\nwith a\nbone");
     }
 
     private String wrap(String s, int length) {
