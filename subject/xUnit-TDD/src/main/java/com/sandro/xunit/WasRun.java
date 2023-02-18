41a8c3a44c04143207c5faf27b102c261cebfffb
Sandro Park <ticoba@naver.com>
2023-02-19 08:26:33 +0900

    TDD로 xUnit 만들기 : 메서드 호출 순서 검증
    - setUp이 testMethod보다 먼저 호출되었는지 검증하는 로직을 추가했다.

--- subject/xUnit-TDD/src/main/java/com/sandro/xunit/WasRun.java
+++ subject/xUnit-TDD/src/main/java/com/sandro/xunit/WasRun.java
@@ -3,6 +3,7 @@ package com.sandro.xunit;
 public class WasRun extends TestCase {
     public boolean wasRun;
     public boolean wasSetUp;
+    public String log;
 
     public WasRun(String name) {
         super(name);
@@ -11,10 +12,12 @@ public class WasRun extends TestCase {
     @Override
     protected void setUp() {
         wasSetUp = true;
+        log = "setUp";
     }
 
     public void testMethod() {
         wasRun = true;
+        log += " testMethod";
     }
 
 }
