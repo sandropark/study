ac1e2a7df7707194bae0d3bd2eb0640046fed99b
Sandro Park <ticoba@naver.com>
2023-02-18 17:31:39 +0900

    Prime factors : 5,6,7 추가 및 리팩토링

--- subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
@@ -15,6 +15,9 @@ class PrimeFactorsTest {
         assertPrimeFactors(2, list(2));
         assertPrimeFactors(3, list(3));
         assertPrimeFactors(4, list(2, 2));
+        assertPrimeFactors(5, list(5));
+        assertPrimeFactors(6, list(2, 3));
+        assertPrimeFactors(7, list(7));
     }
 
     private List<Integer> of(int n) {
@@ -24,9 +27,9 @@ class PrimeFactorsTest {
                 factors.add(2);
                 n /= 2;
             }
-            if (n > 1)
-                factors.add(n);
         }
+        if (n > 1)
+            factors.add(n);
         return factors;
     }
 
