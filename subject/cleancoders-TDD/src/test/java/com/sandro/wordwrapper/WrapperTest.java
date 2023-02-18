cc571f716750eeb52831afdd360e192eb4118f01
Sandro Park <ticoba@naver.com>
2023-02-18 17:53:22 +0900

    WordWrapper : 문자와 길이를 입력하면 길이 뒤 개행문자를 추가해서 반환한다.

--- subject/cleancoders-TDD/src/test/java/com/sandro/wordwrapper/WrapperTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/wordwrapper/WrapperTest.java
@@ -2,9 +2,17 @@ package com.sandro.wordwrapper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class WrapperTest {
+
     @Test
     void test() throws Exception {
-
+        assertThat(wrap("word word", 4)).isEqualTo("word\nword");
     }
+
+    private String wrap(String s, int length) {
+        return s.replaceAll(" ", "\n");
+    }
+
 }
