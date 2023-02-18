05c49af81cf40ee1a72e58056b557d00c42b09f7
Sandro Park <ticoba@naver.com>
2023-02-18 17:17:03 +0900

    Prime factors : 1은 아무것도 반환하지 않는다.

--- subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/primeFactors/PrimeFactorsTest.java
@@ -2,11 +2,19 @@ package com.sandro.primeFactors;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class PrimeFactorsTest {
 
     @Test
-    void test() throws Exception {
+    void canFactorIntoPrimes() throws Exception {
+        assertThat(of(1)).isEqualTo(List.of());
+    }
 
+    private List<Integer> of(int n) {
+        return List.of();
     }
 
 }
