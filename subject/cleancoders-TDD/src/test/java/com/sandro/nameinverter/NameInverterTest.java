00c90a8ebc2fc2da6521709407f6c16a450f1db7
Sandro Park <ticoba@naver.com>
2023-02-20 07:47:55 +0900

    cleancoders/nameinverter : 새로운 케이스를 추가.
    새로운 테스트를 통과하기 위해서 자료구조를 변경했다. 먼저 기존 테스트가 잘 도는지 확인한다.

--- subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
@@ -2,6 +2,9 @@ package com.sandro.nameinverter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NameInverterTest {
@@ -14,16 +17,17 @@ public class NameInverterTest {
         assertThat(invert("first last")).isEqualTo("last, first");
         assertThat(invert("   name   ")).isEqualTo("name");
         assertThat(invert("first     last")).isEqualTo("last, first");
+//        assertThat(invert("Mr. first last")).isEqualTo("last, first");
     }
 
     private String invert(String name) {
         if (name == null || name.equals("")) {
             return "";
         }
-        String[] names = name.trim().split("\\s+");
-        if (names.length > 1) {
-            return String.format("%s, %s", names[1], names[0]);
+        List<String> names = Arrays.asList(name.trim().split("\\s+"));
+        if (names.size() > 1) {
+            return String.format("%s, %s", names.get(1), names.get(0));
         }
-        return names[0];
+        return names.get(0);
     }
 }
