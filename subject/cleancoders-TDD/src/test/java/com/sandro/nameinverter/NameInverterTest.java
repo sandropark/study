e71af6b8cf91c083dcff41ff1a426d39173df310
Sandro Park <ticoba@naver.com>
2023-02-20 07:59:00 +0900

    cleancoders/nameinverter : 테스트 통과
    - Arrays.asList는 불변객체이기 때문에 ArrayList로 감싸서 가변객체로 만들어준다.

--- subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
@@ -2,6 +2,7 @@ package com.sandro.nameinverter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,15 +18,18 @@ public class NameInverterTest {
         assertThat(invert("first last")).isEqualTo("last, first");
         assertThat(invert("   name   ")).isEqualTo("name");
         assertThat(invert("first     last")).isEqualTo("last, first");
-//        assertThat(invert("Mr. first last")).isEqualTo("last, first");
+        assertThat(invert("Mr. first last")).isEqualTo("last, first");
     }
 
     private String invert(String name) {
         if (name == null || name.equals("")) {
             return "";
         }
-        List<String> names = Arrays.asList(name.trim().split("\\s+"));
+        List<String> names = new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
         if (names.size() > 1) {
+            if (names.get(0).equals("Mr.")) {
+                names.remove(0);
+            }
             return String.format("%s, %s", names.get(1), names.get(0));
         }
         return names.get(0);
