1ba0e8b98297450dc50f240b30f415c634390443
Sandro Park <ticoba@naver.com>
2023-02-19 09:49:03 +0900

    TDD로 xUnit 만들기 : TestSuite을 구현
    - 여러 테스트를 모아서 한 번에 실행하고 결과를 확인할 수 있다.

--- subject/xUnit-TDD/src/main/java/com/sandro/xunit/TestCaseTest.java
+++ subject/xUnit-TDD/src/main/java/com/sandro/xunit/TestCaseTest.java
@@ -7,13 +7,14 @@ public class TestCaseTest extends TestCase {
 
     public void testTemplateMethod() {
         WasRun wasRun = new WasRun("testMethod");
-        wasRun.run();
+        wasRun.run(new TestResult());
         Assert.assertEquals("setUp testMethod tearDown", wasRun.log);
     }
 
     public void testResult() {
         WasRun wasRun = new WasRun("testMethod");
-        TestResult result = wasRun.run();
+        TestResult result = new TestResult();
+        wasRun.run(result);
         Assert.assertEquals("1 run, 0 failed", result.getSummary());
     }
 
@@ -26,7 +27,8 @@ public class TestCaseTest extends TestCase {
 
     public void testFailedResult() {
         WasRun wasRun = new WasRun("testBrokenMethod");
-        TestResult result = wasRun.run();
+        TestResult result = new TestResult();
+        wasRun.run(result);
         Assert.assertEquals("1 run, 1 failed", result.getSummary());
     }
 
@@ -34,7 +36,8 @@ public class TestCaseTest extends TestCase {
         TestSuite suite = new TestSuite();
         suite.add(new WasRun("testMethod"));
         suite.add(new WasRun("testBrokenMethod"));
-        TestResult result = suite.run();
+        TestResult result = new TestResult();
+        suite.run(result);
         Assert.assertEquals("2 run, 1 failed", result.getSummary());
     }
 
--- subject/xUnit-TDD/src/main/java/com/sandro/xunit/TestSuite.java
+++ subject/xUnit-TDD/src/main/java/com/sandro/xunit/TestSuite.java
@@ -1,11 +1,16 @@
 package com.sandro.xunit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestSuite {
+    private final List<WasRun> tests = new ArrayList<>();
 
     public void add(WasRun test) {
+        tests.add(test);
     }
 
-    public TestResult run() {
-        return null;
+    public void run(TestResult result) {
+        tests.forEach(t -> t.run(result));
     }
 }
