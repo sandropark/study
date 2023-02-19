27dcceb094d928a32d191f80c8bd4137cc0e9b96
Sandro Park <ticoba@naver.com>
2023-02-20 07:28:00 +0900

    cleancoders/nameinverter : 처음 실패하는 테스트 작성

--- subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
@@ -2,10 +2,16 @@ package com.sandro.nameinverter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NameInverterTest {
 
     @Test
     void name() throws Exception {
+        assertThat(invert(null)).isEqualTo("");
+    }
 
+    private String invert(String name) {
+        return null;
     }
 }
