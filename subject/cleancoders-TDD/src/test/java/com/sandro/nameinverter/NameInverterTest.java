eda1059a1097537e34c300629f8727a605ed91d3
Sandro Park <ticoba@naver.com>
2023-02-20 09:40:53 +0900

    cleancoders/nameinverter : [Refactor] 클래스로 분리

--- subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
+++ subject/cleancoders-TDD/src/test/java/com/sandro/nameinverter/NameInverterTest.java
@@ -10,48 +10,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NameInverterTest {
 
+    NameInverter nameInverter = new NameInverter();
+
     @Test
     void name() throws Exception {
-        assertThat(invert(null)).isEqualTo("");
-        assertThat(invert("")).isEqualTo("");
-        assertThat(invert("name")).isEqualTo("name");
-        assertThat(invert("first last")).isEqualTo("last, first");
-        assertThat(invert("   name   ")).isEqualTo("name");
-        assertThat(invert("first     last")).isEqualTo("last, first");
-        assertThat(invert("Mr. first last")).isEqualTo("last, first");
-        assertThat(invert("Mrs. first last")).isEqualTo("last, first");
-        assertThat(invert("first last SR.")).isEqualTo("last, first SR.");
-        assertThat(invert("first last BS. Phd.")).isEqualTo("last, first BS. Phd.");
+        assertThat(nameInverter.invert(null)).isEqualTo("");
+        assertThat(nameInverter.invert("")).isEqualTo("");
+        assertThat(nameInverter.invert("name")).isEqualTo("name");
+        assertThat(nameInverter.invert("first last")).isEqualTo("last, first");
+        assertThat(nameInverter.invert("   name   ")).isEqualTo("name");
+        assertThat(nameInverter.invert("first     last")).isEqualTo("last, first");
+        assertThat(nameInverter.invert("Mr. first last")).isEqualTo("last, first");
+        assertThat(nameInverter.invert("Mrs. first last")).isEqualTo("last, first");
+        assertThat(nameInverter.invert("first last SR.")).isEqualTo("last, first SR.");
+        assertThat(nameInverter.invert("first last BS. Phd.")).isEqualTo("last, first BS. Phd.");
     }
 
-    private String invert(String name) {
-        if (isEmpty(name)) return "";
-        return formatName(removeHonorific(splitNames(name)));
-    }
+    public class NameInverter {
 
-    private boolean isEmpty(String name) {
-        return name == null || name.equals("");
-    }
+        public NameInverter() {}
 
-    private ArrayList<String> splitNames(String name) {
-        return new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
-    }
+        public String invert(String name) {
+            if (isEmpty(name)) return "";
+            return formatName(removeHonorific(splitNames(name)));
+        }
 
-    private List<String> removeHonorific(List<String> names) {
-        if (isHonorific(names)) names.remove(0);
-        return names;
-    }
+        private boolean isEmpty(String name) {
+            return name == null || name.equals("");
+        }
 
-    private boolean isHonorific(List<String> names) {
-        return names.get(0).matches("Mr\\.|Mrs\\.");
-    }
+        private ArrayList<String> splitNames(String name) {
+            return new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
+        }
 
-    private String formatName(List<String> names) {
-        if (names.size() == 1) return names.get(0);
-        return String.format("%s, %s %s", names.get(1), names.get(0), postNominal(names)).trim();
-    }
+        private List<String> removeHonorific(List<String> names) {
+            if (isHonorific(names)) names.remove(0);
+            return names;
+        }
 
-    private String postNominal(List<String> names) {
-        return String.join(" ", names.subList(2, names.size()));
+        private boolean isHonorific(List<String> names) {
+            return names.get(0).matches("Mr\\.|Mrs\\.");
+        }
+
+        private String formatName(List<String> names) {
+            if (names.size() == 1) return names.get(0);
+            return String.format("%s, %s %s", names.get(1), names.get(0), postNominal(names)).trim();
+        }
+
+        private String postNominal(List<String> names) {
+            return String.join(" ", names.subList(2, names.size()));
+        }
     }
 }
