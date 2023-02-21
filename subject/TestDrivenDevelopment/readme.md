de4e32722efc926864da3898c37f3610a82fc68e
Sandro Park <ticoba@naver.com>
2023-02-21 12:52:43 +0900

    TDD/Money : [Blue] reduceMoneyDifferentCurrency
    - Bank에서 환율을 저장하고 조회할 수 있게 수정

--- subject/TestDrivenDevelopment/readme.md
+++ subject/TestDrivenDevelopment/readme.md
@@ -4,11 +4,11 @@
 
 ## TODO
 * [ ] $5 + 10CHF = $10(환율이 2:1인 경우)
-* [ ] $5 + $5 = $10
+* [x] $5 + $5 = $10
 * [ ] $5 + $5에서 Money 반환하기
 * [x] Bank.reduce(Money)
-* [ ] Money에 대한 통화 변환을 수행하는 Reduce
-* [ ] Reduce(Bank, String)
+* [x] Money에 대한 통화 변환을 수행하는 Reduce
+* [x] Reduce(Bank, String)
 * [x] $5 x 2 = $10
 * [x] amount를 private로 만들기
 * [x] Dollar 부작용(side effect?)
--- subject/TestDrivenDevelopment/src/main/java/com/sandro/money/Bank.java
+++ subject/TestDrivenDevelopment/src/main/java/com/sandro/money/Bank.java
@@ -1,17 +1,43 @@
 package com.sandro.money;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            Pair pair = (Pair) o;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     public void addRate(String from, String to, int rate) {
-
+        rates.put(new Pair(from, to), rate);
     }
 
     public int rate(String from, String to) {
-        return from.equals("CHF") && to.equals("USD")
-                ? 2
-                : 1;
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate;
     }
 }
