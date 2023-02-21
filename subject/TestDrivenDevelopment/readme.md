b26c7e7859bfdb47acf9ca40261b229d2ef58c30
Sandro Park <ticoba@naver.com>
2023-02-21 09:33:08 +0900

    TDD/Money : [Blue] multiplication

--- subject/TestDrivenDevelopment/readme.md
+++ subject/TestDrivenDevelopment/readme.md
@@ -4,7 +4,7 @@
 
 ## TODO
 * [ ] $5 + 10CHF = $10(환율이 2:1인 경우)
-* [ ] $5 x 2 = $10
+* [x] $5 x 2 = $10
 * [ ] amount를 private로 만들기
 * [ ] Dollar 부작용(side effect?)
 * [ ] Money 반올림?
--- subject/TestDrivenDevelopment/src/main/java/com/sandro/money/Dollar.java
+++ subject/TestDrivenDevelopment/src/main/java/com/sandro/money/Dollar.java
@@ -1,12 +1,13 @@
 package com.sandro.money;
 
 public class Dollar {
-    public int amount = 10;
+    public int amount;
 
-    public Dollar(int i) {
+    public Dollar(int amount) {
+        this.amount = amount;
     }
 
-    public void times(int i) {
-
+    public void times(int multiplier) {
+        amount *= multiplier;
     }
 }
