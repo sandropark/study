86984bd4632149e1d5f3312b3d6aeb06b97f0a73
Sandro Park <ticoba@naver.com>
2023-02-24 09:54:59 +0900

    테스트 메서드 호출하기 - Blue
    - 테스트 메서드를 직접 실행하지 않고 인터페이스인 `run()`을 사용한다.

--- test-driven-development-xUnit/main.py
+++ test-driven-development-xUnit/main.py
@@ -5,8 +5,11 @@ class WasRun:
     def test_method(self):
         self.was_run = 1
 
+    def run(self):
+        self.test_method()
+
 
 test = WasRun("testMethod")
 print(test.was_run)
-test.test_method()
+test.run()
 print(test.was_run)
