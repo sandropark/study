3cf215676c60fba7da27353cb494d4c4bccaddf3
Sandro Park <ticoba@naver.com>
2023-02-24 09:50:55 +0900

    테스트 메서드 호출하기 - Red

--- test-driven-development-xUnit/main.py
+++ test-driven-development-xUnit/main.py
@@ -1,1 +1,14 @@
+class WasRun:
+    def __init__(self, name):
+        self.was_run = None
 
+    pass
+
+    def test_method(self):
+        pass
+
+
+test = WasRun("testMethod")
+print(test.was_run)
+test.test_method()
+print(test.was_run)
