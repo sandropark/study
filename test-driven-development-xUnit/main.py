9bfd50a7c310ac8004a10083d6681738a56bc028
Sandro Park <ticoba@naver.com>
2023-02-24 09:57:44 +0900

    테스트 메서드 호출하기 - Blue
    - 객체를 생성할 때 넘기는 메서드 명을 사용해서 동적으로 메서드가 호출되게 수정

--- test-driven-development-xUnit/main.py
+++ test-driven-development-xUnit/main.py
@@ -1,15 +1,17 @@
 class WasRun:
     def __init__(self, name):
         self.was_run = None
+        self.name = name
 
     def test_method(self):
         self.was_run = 1
 
     def run(self):
-        self.test_method()
+        method = getattr(self, self.name)
+        method()
 
 
-test = WasRun("testMethod")
+test = WasRun("test_method")
 print(test.was_run)
 test.run()
 print(test.was_run)
