9113c2f5ac1d5e92dc34356e3b948026a3001b01
Sandro Park <ticoba@naver.com>
2023-02-25 21:00:36 +0900

    숫자가 0보다 작다면 예외가 발생한다. : Red

--- java/subject/TestDrivenDevelopment/src/test/java/com/sandro/triangle/TriangleTest.java
+++ java/subject/TestDrivenDevelopment/src/test/java/com/sandro/triangle/TriangleTest.java
@@ -1,11 +1,23 @@
 package com.sandro.triangle;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TriangleTest {
-    @Test
-    void test() throws Exception {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+public class TriangleTest {
+
+    @DisplayName("숫자가 0보다 작다면 예외가 발생한다.")
+    @Test
+    void argumentsShouldGreaterThan0() throws Exception {
+        assertThatThrownBy(() -> new Triangle(0, 0, 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private class Triangle {
+        public Triangle(int a, int b, int c) {
+
+        }
 
     }
 }
