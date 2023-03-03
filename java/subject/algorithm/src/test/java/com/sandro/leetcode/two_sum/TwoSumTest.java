4553a3994c122afac29f4ffffcf0e4204925f4f7
Sandro Park <ticoba@naver.com>
2023-03-03 18:38:24 +0900

    Two Sum : Green - 성공

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/two_sum/TwoSumTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/two_sum/TwoSumTest.java
@@ -13,7 +13,15 @@ public class TwoSumTest {
     }
 
     private int[] twoSum(int[] nums, int target) {
-        return new int[]{0, 1};
+        for (int i = 0; i < nums.length; i++) {
+            int augend = nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                int addend = nums[j];
+                if (i != j && augend + addend == target)
+                    return new int[]{i, j};
+            }
+        }
+        throw new IllegalArgumentException();
     }
 
 }
