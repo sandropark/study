94a7ccca8cc40d459d7dcc96dc03ad5512e1bf0d
Sandro Park <ticoba@naver.com>
2023-03-03 18:32:16 +0900

    Two Sum : Red

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/two_sum/TwoSumTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/two_sum/TwoSumTest.java
@@ -9,6 +9,7 @@ public class TwoSumTest {
     @Test
     void twoSumTest() throws Exception {
         assertThat(twoSum(new int[]{0, 0}, 0)).isEqualTo(new int[]{0, 1});
+        assertThat(twoSum(new int[]{1, 0, 0}, 0)).isEqualTo(new int[]{1, 2});
     }
 
     private int[] twoSum(int[] nums, int target) {
