5802059516acf9a48437dd4bd00d9645d12d3b3f
Sandro Park <ticoba@naver.com>
2023-03-05 22:46:12 +0900

    Roman to Integer : Red

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
@@ -1,4 +1,17 @@
 package com.sandro.leetcode.roman_to_integer;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RomanToIntegerTest {
+
+    @Test
+    void romanToIntTest() throws Exception {
+        assertThat(romanToInt("I")).isEqualTo(1);
+    }
+
+    private int romanToInt(String s) {
+        return 0;
+    }
 }
