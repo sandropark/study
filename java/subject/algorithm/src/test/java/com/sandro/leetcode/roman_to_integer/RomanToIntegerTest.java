0db67c412d094e0ced7e77b853d83b6eb07cfa12
Sandro Park <ticoba@naver.com>
2023-03-06 22:12:11 +0900

    Roman to Integer : Green

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
@@ -2,6 +2,7 @@ package com.sandro.leetcode.roman_to_integer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,10 +24,14 @@ public class RomanToIntegerTest {
 
     private int romanToInt(String s) {
         Map<String, Integer> map = Map.of("I", 1, "V", 5, "X", 10, "L", 50, "C", 100, "D", 500, "M", 1000);
-        int result = 0;
         String[] strings = s.split("");
-        for (String str : strings) {
-            result += map.get(str);
+        int result = map.get(strings[0]);
+        for (int i = 1; i < strings.length; i++) {
+            String preValue = strings[i - 1];
+            String tempValue = strings[i];
+            if (preValue.equals("I") && List.of("V", "X").contains(tempValue))
+                result *= -1;
+            result += map.get(tempValue);
         }
         return result;
     }
