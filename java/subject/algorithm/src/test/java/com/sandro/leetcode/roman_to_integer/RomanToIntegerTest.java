9015db29bf0aac5e1e00653d404740f009e1787c
Sandro Park <ticoba@naver.com>
2023-03-06 22:56:35 +0900

    Roman to Integer : 다시 해보기 Green

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
@@ -17,38 +17,26 @@ public class RomanToIntegerTest {
         assertThat(romanToInt("M")).isEqualTo(1000);
         assertThat(romanToInt("II")).isEqualTo(2);
         assertThat(romanToInt("IV")).isEqualTo(4);
-//        assertThat(romanToInt("IX")).isEqualTo(9);
-//        assertThat(romanToInt("MCMXCIV")).isEqualTo(1994);
+        assertThat(romanToInt("IX")).isEqualTo(9);
+        assertThat(romanToInt("MCMXCIV")).isEqualTo(1994);
     }
 
     private int romanToInt(String s) {
         int num = 0;
         int result = 0;
+        int tmp = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             switch (s.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
+                case 'I': num = 1;    break;
+                case 'V': num = 5;    break;
+                case 'X': num = 10;   break;
+                case 'L': num = 50;   break;
+                case 'C': num = 100;  break;
+                case 'D': num = 500;  break;
+                case 'M': num = 1000; break;
             }
-            result += num;
+            result = num < tmp ? result - num : result + num;
+            tmp = num;
         }
         return result;
     }
