8ea05ea4f8339516f600e9c80aa4db2ab115bbfb
Sandro Park <ticoba@naver.com>
2023-03-06 23:01:43 +0900

    Roman to Integer : 다시 해보기 Blue

--- java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/roman_to_integer/RomanToIntegerTest.java
@@ -2,42 +2,42 @@ package com.sandro.leetcode.roman_to_integer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanToIntegerTest {
 
+    RomanConvertor convertor = new RomanConvertor();
+
     @Test
     void romanToIntTest() throws Exception {
-        assertThat(romanToInt("I")).isEqualTo(1);
-        assertThat(romanToInt("V")).isEqualTo(5);
-        assertThat(romanToInt("X")).isEqualTo(10);
-        assertThat(romanToInt("L")).isEqualTo(50);
-        assertThat(romanToInt("C")).isEqualTo(100);
-        assertThat(romanToInt("D")).isEqualTo(500);
-        assertThat(romanToInt("M")).isEqualTo(1000);
-        assertThat(romanToInt("II")).isEqualTo(2);
-        assertThat(romanToInt("IV")).isEqualTo(4);
-        assertThat(romanToInt("IX")).isEqualTo(9);
-        assertThat(romanToInt("MCMXCIV")).isEqualTo(1994);
+        assertThat(convertor.romanToInt("I")).isEqualTo(1);
+        assertThat(convertor.romanToInt("V")).isEqualTo(5);
+        assertThat(convertor.romanToInt("X")).isEqualTo(10);
+        assertThat(convertor.romanToInt("L")).isEqualTo(50);
+        assertThat(convertor.romanToInt("C")).isEqualTo(100);
+        assertThat(convertor.romanToInt("D")).isEqualTo(500);
+        assertThat(convertor.romanToInt("M")).isEqualTo(1000);
+        assertThat(convertor.romanToInt("II")).isEqualTo(2);
+        assertThat(convertor.romanToInt("IV")).isEqualTo(4);
+        assertThat(convertor.romanToInt("IX")).isEqualTo(9);
+        assertThat(convertor.romanToInt("MCMXCIV")).isEqualTo(1994);
     }
 
-    private int romanToInt(String s) {
-        int num = 0;
-        int result = 0;
-        int tmp = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I': num = 1;    break;
-                case 'V': num = 5;    break;
-                case 'X': num = 10;   break;
-                case 'L': num = 50;   break;
-                case 'C': num = 100;  break;
-                case 'D': num = 500;  break;
-                case 'M': num = 1000; break;
+    private static class RomanConvertor {
+        private final Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+
+        private int romanToInt(String s) {
+            int num;
+            int result = 0;
+            int tmp = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                num = map.get(s.charAt(i));
+                result = num < tmp ? result - num : result + num;
+                tmp = num;
             }
-            result = num < tmp ? result - num : result + num;
-            tmp = num;
+            return result;
         }
-        return result;
     }
 }
