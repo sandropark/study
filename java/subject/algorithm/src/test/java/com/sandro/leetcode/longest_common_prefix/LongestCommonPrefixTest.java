8d73b2960f8a3240f133e2c2eaa7e0d8846b2ec3
Sandro Park <ticoba@naver.com>
2023-03-07 21:15:59 +0900

    Longest Common Prefix : Red

---
+++ java/subject/algorithm/src/test/java/com/sandro/leetcode/longest_common_prefix/LongestCommonPrefixTest.java
@@ -0,0 +1,17 @@
+package com.sandro.leetcode.longest_common_prefix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestCommonPrefixTest {
+
+    @Test
+    void testLongestCommonPrefix() throws Exception {
+        assertThat(longestCommonPrefix(new String[]{""})).isEqualTo("");
+    }
+
+    private String longestCommonPrefix(String[] strings) {
+        return null;
+    }
+}
