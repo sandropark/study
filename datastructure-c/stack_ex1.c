dfcb35a60c60c86884cbe8c807cbd30e21f017cb
Sandro Park <ticoba@naver.com>
2023-04-02 10:05:45 +0900

    Stack - prefix 계산기 구현

--- datastructure-c/stack_ex1.c
+++ datastructure-c/stack_ex1.c
@@ -66,23 +66,23 @@ int getPriority(char operator)
 {
     if (operator == '*' || operator == '/') return 0;
     if (operator == '+' || operator == '-') return 1;
-    return  2;
+    return 2;
 }
 
-int isTopSmaller(char tmpElement, NODE *pTop)
+int isTopSmaller(char operator, NODE *pTop)
 {
-    if (pTop == NULL || tmpElement == '(') return 0;
-    if (tmpElement == ')') return 1;
-    return getPriority(tmpElement) >= getPriority(pTop->data[0]);
+    if (pTop == NULL || operator == '(') return 0; // 스택이 비어있거나 현재 연산자가 '('인 경우 push 한다.
+    if (operator == ')') return 1;                 // 현재 연산자가 ')'인 경우 계속 pop한다.
+    return getPriority(operator) >= getPriority(pTop->data[0]);
 }
 
-void infixToPostfixSolution(char *input)
+LIST *infixToPostfixSolution(char *input)
 {
     LIST operators = {0};
     initList(&operators);
 
-    LIST result = {0};
-    initList(&result);
+    LIST *result = malloc(sizeof(LIST));
+    initList(result);
 
     for (int i = 0; i < strlen(input); i++)
     {
@@ -94,7 +94,7 @@ void infixToPostfixSolution(char *input)
             {
                 char *pPop = pop(&operators)->data;
                 if (strcmp(pPop, "(") != 0)
-                    insertTail(&result, pPop);
+                    insertTail(result, pPop);
                 else
                     break;
             }
@@ -102,44 +102,104 @@ void infixToPostfixSolution(char *input)
                 push(&operators, str);
         }
         else
-            insertTail(&result, str);
+            insertTail(result, str);
     }
     while (isNotEmpty(&operators))
-        insertTail(&result, pop(&operators)->data);
-    printStack(&result);
+        insertTail(result, pop(&operators)->data);
+    return result;
 }
 
 void infixToPostfix()
 {
-    printf("계산기\n");
+    printf("infix to postfix\n");
 
     char *input = "1+1";
     printf("input = %s : 예상=11+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "1*1";
     printf("input = %s : 예상=11* 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "1+1+1";
     printf("input = %s : 예상=11+1+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "1+1*1";
     printf("input = %s : 예상=111*+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "1*1+1";
     printf("input = %s : 예상=11*1+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "1*1*1+1";
     printf("input = %s : 예상=11*1*1+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
 
     input = "6+(3-2)*4";
     printf("input = %s : 예상=632-4*+ 실제=", input);
-    infixToPostfixSolution(input);
+    printStack(infixToPostfixSolution(input));
+}
+
+int calculate(char *n1, char *n2, char operator)
+{
+    if (operator == '+')
+        return atoi(n1) + atoi(n2);
+    if (operator == '-')
+        return atoi(n1) - atoi(n2);
+    if (operator == '*')
+        return atoi(n1) * atoi(n2);
+    if (operator == '/')  
+        return atoi(n1) / atoi(n2);
+    return 0;
+}
+
+int getResult(char *input)
+{
+    LIST stack = {0};
+    initList(&stack);
+
+    LIST *list = infixToPostfixSolution(input);
+
+    while (isNotEmpty(list))
+    {
+        char *pTmp = pop(list)->data;
+        
+        if (isOperator(pTmp[0]))
+        {
+            char operator = pTmp[0];
+            char *n2 = pop(&stack)->data;
+            char *n1 = pop(&stack)->data;
+            char result[64];
+            sprintf(result, "%d", calculate(n1, n2, operator));
+            push(&stack, result);
+        }
+        else
+            push(&stack, pTmp);
+    }
+    return atoi(pop(&stack)->data);
+}
+
+void calculateTest()
+{
+    char *input = "1+1";
+    printf("input = %s : 예상=2 실제=%d\n", input, getResult(input));
+
+    input = "2*2";
+    printf("input = %s : 예상=4 실제=%d\n", input, getResult(input));
+
+    input = "1+1+1";
+    printf("input = %s : 예상=3 실제=%d\n", input, getResult(input));
+
+    input = "3*3+3";
+    printf("input = %s : 예상=12 실제=%d\n", input, getResult(input));
+
+    input = "3+3*3";
+    printf("input = %s : 예상=12 실제=%d\n", input, getResult(input));
+
+    input = "6+(3-2)*4";
+    printf("input = %s : 예상=10 실제=%d\n", input, getResult(input));
 }
 
 int main(int argc, char const *argv[])
@@ -148,7 +208,10 @@ int main(int argc, char const *argv[])
     // test1();
 
     // 2. infix to postfix
-    infixToPostfix();
+    // infixToPostfix();
+
+    // 3. 계산기
+    calculateTest();
 
     return 0;
 }
