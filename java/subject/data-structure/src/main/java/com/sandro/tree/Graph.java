c4b5ed5bd88361d42332779bfbc92b2b9cf32e81
sandropark <ticoba@naver.com>
2023-05-05 18:46:00 +0900

    DFS - 실습

---
+++ java/subject/data-structure/src/main/java/com/sandro/tree/Graph.java
@@ -0,0 +1,92 @@
+package com.sandro.tree;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Graph<T> {
+
+    private final List<DfsNode<T>> dfsNodes = new ArrayList<>();
+    @Getter
+    private DfsNode<T> root;
+    private static int currentTime = 1;
+
+    public Graph(DfsNode<T>... dfsNodes) {
+        this.dfsNodes.addAll(Arrays.stream(dfsNodes).collect(Collectors.toList()));
+        if (dfsNodes.length != 0)
+            root = dfsNodes[0];
+    }
+
+    public List<String> dfs() {
+        if (root == null)
+            throw new IllegalStateException("노드가 없습니다.");
+        ArrayList<String> result = new ArrayList<>();
+        root.dfs(result);
+        return result;
+    }
+
+    static class DfsNode<T> {
+        private final List<DfsNode<T>> neighbors = new ArrayList<>();
+        @Getter
+        private final T key;
+        private int preTime;
+        private int postTime;
+        @Setter
+        private DfsNode<T> parent;
+        private final List<DfsNode<T>> children = new ArrayList<>();
+
+        public DfsNode(T key) {
+            this.key = key;
+        }
+
+        public void addNeighbors(DfsNode<T>... neighbors) {
+            this.neighbors.addAll(Arrays.stream(neighbors).collect(Collectors.toList()));
+        }
+
+        public void dfs(List<String> result) {
+            result.add(key.toString());
+            preTime = currentTime++;
+            System.out.println("key = " + key + " , preTime = " + preTime);
+            for (DfsNode<T> neighbor : neighbors) {
+                if (neighbor.hasNotMet(result)) {
+                    neighbor.dfs(result);
+                    neighbor.setParent(this);
+                    children.add(neighbor);
+                }
+            }
+            postTime = currentTime++;
+            System.out.println("key = " + key + " , postTime = " + postTime);
+        }
+
+        private boolean hasNotMet(List<String> result) {
+            return !result.contains(key.toString());
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java/subject/data-structure/src/test/java/com/sandro/tree/GraphTest.java
@@ -0,0 +1,56 @@
+package com.sandro.tree;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GraphTest {
+
+    @Test
+    void dfs() throws Exception {
+        Graph.DfsNode<String> a = new Graph.DfsNode<>("a");
+        Graph.DfsNode<String> b = new Graph.DfsNode<>("b");
+        Graph.DfsNode<String> c = new Graph.DfsNode<>("c");
+        Graph.DfsNode<String> d = new Graph.DfsNode<>("d");
+
+        a.addNeighbors(b, d);
+        b.addNeighbors(c);
+
+        Graph<String> graph = new Graph<>(a, b, c, d);
+
+        List<String> dfs = graph.dfs();
+
+        assertThat(dfs).isEqualTo(List.of("a", "b", "c", "d"));
+    }
+
+    @Test
+    void dfs2() throws Exception {
+        Graph.DfsNode<String> a = new Graph.DfsNode<>("a");
+        Graph.DfsNode<String> b = new Graph.DfsNode<>("b");
+        Graph.DfsNode<String> c = new Graph.DfsNode<>("c");
+        Graph.DfsNode<String> d = new Graph.DfsNode<>("d");
+        Graph.DfsNode<String> e = new Graph.DfsNode<>("e");
+        Graph.DfsNode<String> f = new Graph.DfsNode<>("f");
+        Graph.DfsNode<String> g = new Graph.DfsNode<>("g");
+        Graph.DfsNode<String> h = new Graph.DfsNode<>("h");
+
+        a.addNeighbors(b, c);
+        b.addNeighbors(a, c, e, h);
+        c.addNeighbors(a, b, d);
+        d.addNeighbors(c, f);
+        e.addNeighbors(b, g);
+        f.addNeighbors(d);
+        g.addNeighbors(e);
+        h.addNeighbors(b);
+
+        Graph<String> graph = new Graph<>(a, b, c, d, e, f, g, h);
+
+        List<String> dfs = graph.dfs();
+
+        assertThat(dfs).isEqualTo(List.of("a", "b", "c", "d", "f", "e", "g", "h"));
+
+    }
+
+}
