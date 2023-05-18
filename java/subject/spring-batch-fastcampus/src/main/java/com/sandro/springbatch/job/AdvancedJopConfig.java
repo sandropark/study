c603819f3914716e2e7be73fb896593c4487672f
sandropark <ticoba@naver.com>
2023-05-18 15:59:27 +0900

    스프링 배치 - JobExecutionListener 테스트
    - job 실행 전, 후 필요한 작업을 할 수 있다. 상태에 따라서 원하는 작업을 넣을 수도 있다.

--- java/subject/spring-batch-fastcampus/src/main/java/com/sandro/springbatch/job/AdvancedJopConfig.java
+++ java/subject/spring-batch-fastcampus/src/main/java/com/sandro/springbatch/job/AdvancedJopConfig.java
@@ -3,8 +3,7 @@ package com.sandro.springbatch.job;
 import com.sandro.springbatch.job.validator.LocalDateParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -27,12 +26,31 @@ public class AdvancedJopConfig {
     private final String parameterName = "targetDate";
 
     @Bean
-    public Job advancedJob(Step advancedStep) {
+    public Job advancedJob(JobExecutionListener jobExecutionListener, Step advancedStep) {
         return jobBuilderFactory.get("advancedJob")
-                .validator(new LocalDateParameterValidator(parameterName))
                 .incrementer(new RunIdIncrementer())
+                .validator(new LocalDateParameterValidator(parameterName))
+                .listener(jobExecutionListener)
                 .start(advancedStep)
                 .build();
+    }
+
+    @JobScope
+    @Bean
+    public JobExecutionListener jobExecutionListener() {
+        return new JobExecutionListener() {
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                log.info("[JobExecutionListener#beforeJob] jobExecution is {}", jobExecution.getStatus());
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+//                log.info("[JobExecutionListener#afterJob] jobExecution is {}", jobExecution.getStatus());
+                if (jobExecution.getStatus() == BatchStatus.FAILED) // 상태에 따라서 원하는 작업을 수행할 수 있다.
+                    log.error("[JobExecutionListener#afterJob] jobExecution is Failed");
+            }
+        };
     }
 
     @JobScope
@@ -48,8 +66,10 @@ public class AdvancedJopConfig {
     @Bean
     public Tasklet advancedTasklet(@Value("#{jobParameters['" + parameterName + "']}") String targetDate) {
         return (contribution, chunkContext) -> {
-            log.info("[AdvancedJobConfig] JobParameter - targetDate = " + targetDate);
+            log.info("[AdvancedJobConfig] JobParameter - targetDate = {}", targetDate);
             LocalDate executionDate = LocalDate.parse(targetDate);
+            if (executionDate.isBefore(LocalDate.of(2021, 01, 02)))
+                throw new RuntimeException("에러 발생");
             log.info("[AdvancedJobConfig] excuted advancedTasklet");
             return RepeatStatus.FINISHED;
         };
--- java/subject/spring-batch-fastcampus/src/test/java/com/sandro/springbatch/job/AdvancedJopConfigTest.java
+++ java/subject/spring-batch-fastcampus/src/test/java/com/sandro/springbatch/job/AdvancedJopConfigTest.java
@@ -3,9 +3,7 @@ package com.sandro.springbatch.job;
 import com.sandro.springbatch.BatchTestConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
@@ -34,6 +33,16 @@ class AdvancedJopConfigTest {
         assertThatThrownBy(() -> jobLauncherTestUtils.launchJob(jobParameters))
                 .isInstanceOf(JobParametersInvalidException.class)
                 .message().contains("유효하지 않은 날짜");
+    }
+
+    @DisplayName("jobExecutionStatus 확인")
+    @Test
+    void status() throws Exception {
+        JobParameters jobParameters = new JobParameters(Map.of("targetDate", new JobParameter("2021-01-01")));
+
+        JobExecution execution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        assertThat(execution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode());
     }
 
 }
