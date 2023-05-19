4b8183e92f9b9f20128d74ef859627209e7e6a76
sandropark <ticoba@naver.com>
2023-05-19 14:58:00 +0900

    스프링 배치 - 파일 테스트
    - 배치를 통해서 파일을 생성한다면 미리 결과물을 생성해두고 테스트를 통해서 결과물과 비교할 수 있다.

--- java/subject/spring-batch-fastcampus/src/test/java/com/sandro/springbatch/job/player/FlatFileJobConfigTest.java
+++ java/subject/spring-batch-fastcampus/src/test/java/com/sandro/springbatch/job/player/FlatFileJobConfigTest.java
@@ -2,16 +2,19 @@ package com.sandro.springbatch.job.player;
 
 import com.sandro.springbatch.BatchTestConfig;
 import com.sandro.springbatch.core.service.PlayerSalaryService;
-import com.sandro.springbatch.job.AdvancedJopConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.AssertFile;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,10 +27,12 @@ class FlatFileJobConfigTest {
     @Autowired
     JobLauncherTestUtils jobLauncherTestUtils;
 
+    @DisplayName("파일에서 데이터를 읽고, 데이터를 가공해서 다시 파일로 저장한다.")
     @Test
     void test() throws Exception {
         JobExecution execution = jobLauncherTestUtils.launchJob();
 
         assertThat(execution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
+        AssertFile.assertFileEquals(new File("player-salary-list.txt"), new File("succeed-player-salary-list.txt"));
     }
 }
